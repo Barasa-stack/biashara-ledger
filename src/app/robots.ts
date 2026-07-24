13e91b35b791c17bbadc9c4ee9c1563c4e0df22f
Enock Shimaka Barasa <digitalbaroz@gmail.com>
2026-07-24 12:16:48 +0300

    fix: sitemap domain and add robots.txt for Google indexing

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,12 +2,7 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/admin/', '/api/', '/sign-in/', '/sign-up/', '/renew/'],
-    },
-    sitemap: 'https://biasharaledsgr.com/sitemap.xml',
+    rules: { userAgent: '*', allow: '/' },
+    sitemap: 'https://biasharaledger.qzz.io/sitemap.xml',
   };
 }
-// deployment: Mon Jul  6 08:23:50 EAT 2026
